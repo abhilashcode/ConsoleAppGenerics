4e000ab33347071b09e6e9571a483cf85329f217
Abhilash Jain B <abhilashjainb7@gmail.com>
2021-08-14 22:53:53 +0530

    its again

--- ConsoleAppGenerics/Program.cs
+++ ConsoleAppGenerics/Program.cs
@@ -70,7 +70,7 @@ namespace ConsoleAppGenerics
             int a= ds.GetElement(0);
            // Console.Write("Its a : {0}",a);
            // Console.Write("Its a : {0}",a);
-           // Console.Write("Its a : {0}",a);
+           // Console.Write("Its a : {0}11",a);
         }
     }
 }
